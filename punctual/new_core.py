db49db22a1fc410a2d87038ca88f6770711de56e
EXPERTAI\liacomino <liacomino@expert.ai>
2024-05-31 15:50:15 +0200

    First working version of a refactored code, introducing the Schedule object

--- punctual/new_core.py
+++ punctual/new_core.py
@@ -1,21 +1,53 @@
+from abc import ABC, abstractmethod
 from datetime import datetime
 from datetime import timedelta
-
-from typing import NamedTuple
-from typing import LiteralString
+from typing import Generic
 from typing import List
+from typing import NamedTuple
+from typing import Tuple
+from typing import TypeVar
+from typing import Union
+
+from punctual.core import add_synonym_duration
+from punctual.core import prettify_report
+from punctual.core import parse_entry
+from punctual.core import get_duration
+
+ParsableEntryType = TypeVar('ParsableEntryType')
 
 
-class Moment(NamedTuple):
-    datetime: datetime
-    previous_entry: "Entry"
-    next_entry: "Entry"
+class Parser(ABC, Generic[ParsableEntryType]):
+
+    @abstractmethod
+    def parse(self, entry: Generic[ParsableEntryType], **kwargs) -> Tuple[str, timedelta, Union[datetime, None]]:
+        raise NotImplementedError("To be implemented in subclasses")
+
+
+class StandardParser(Parser):
+
+    def __init__(self, synonyms: List[Tuple[str, int]], contingency: timedelta = None):
+        # the user can specify synonyms: they are like labels with a duration
+        # so that the user can refer to a duration by its label (i.e. synonym)
+        self._synonyms = {}
+        for syn in synonyms:
+            add_synonym_duration(syn[0], self._synonyms, syn[1])
+        self._contingency = contingency if contingency else timedelta(minutes=2)
+
+    def parse(self, entry: Generic[ParsableEntryType], **kwargs) -> Tuple[str, timedelta, Union[datetime, None]]:
+        entry_name, at = parse_entry(entry, kwargs.get('start_time'))
+        entry_duration = get_duration(entry_name, self._synonyms)
+        return entry_name, timedelta(minutes=entry_duration) + self._contingency, at
 
 
 class Entry(NamedTuple):
-    name: LiteralString
-    start_time: Moment
-    end_time: Moment
+    name: str
+    start_time: datetime
+    end_time: datetime
+    duration: timedelta
+
+    @property
+    def minutes(self) -> float:
+        return self.duration.total_seconds() / 60
 
 
 class Schedule:
@@ -26,11 +58,29 @@ class Schedule:
     # CONSTRUCTORS
 
     @classmethod
-    def from_names(cls, *names: str, duration: timedelta) -> "Schedule":
-        result: Schedule = Schedule()
+    def _now(cls) -> datetime:
+        return datetime.now()
+
+    @classmethod
+    def from_names(cls, *names: str, duration: timedelta, start: datetime = None) -> "Schedule":
+        result: Schedule = cls()
         for name in names:
-            result.append(name)
-            print(name)
+            result.append(name, duration, start)
+        return result
+
+    @classmethod
+    def from_entries(cls, *entries: Generic[ParsableEntryType], parser: Parser[Generic[ParsableEntryType]]):
+        result: Schedule = cls()
+        i = 0
+        for entry in entries:
+            name, duration, start = parser.parse(
+                entry,
+                # FIX-20240531: The StandardParser requires start_time to extrapolate
+                # the date (year, month and day) to compose the entry start_time
+                start_time=Schedule._now() if i == 0 else result.last.start_time
+            )
+            result.append(name, duration, start)
+            i = i + 1
         return result
 
     # MAGIC METHODS & PROPERTIES
@@ -38,30 +88,75 @@ class Schedule:
     def __len__(self):
         return len(self._entries)
 
+    def __dict__(self):
+        return {
+            'entries': [entry._asdict() for entry in self._entries],
+            'total_duration_minutes': self.minutes,
+            'start_time': self.start,
+            'end_time': self.end
+        }
+
+    def __str__(self):
+        return prettify_report(self.__dict__())
+
     @property
     def empty(self) -> bool:
         return len(self) == 0
 
-    # METHODS TO ADD ENTRIES
+    @property
+    def first(self) -> Entry:
+        self._raise_error_if_empty()
+        return self._entries[0]
 
-    def append(self, name: str, duration: timedelta) -> "Entry":
+    @property
+    def last(self) -> Entry:
+        self._raise_error_if_empty()
+        return self._entries[-1]
+
+    @property
+    def minutes(self) -> float:
+        return sum([entry.minutes for entry in self._entries])
+
+    @property
+    def start(self) -> datetime:
+        return self.first.start_time
+
+    @property
+    def end(self) -> datetime:
+        return self.last.end_time
+
+    # PRIVATE METHODS
+
+    def _raise_error_if_empty(self):
         if self.empty:
-            start_time = datetime.now()
-            end_time = start_time + duration
-            # TODO Solve this: how to handle the fact that we both have to construct
-            # an Entry and Moment instance and they reference each other?
-            Entry(name=name, )
+            raise IndexError("There are no entries")
 
+    def _start_end_time(self, duration: timedelta, start: datetime = None) -> Tuple[datetime, datetime]:
+        # user may have provided a start time, that's why we check for
+        # start if start else ...
+        if self.empty:
+            current = start if start else Schedule._now()
+            return current, current + duration
+        return start if start else self.last.end_time, self.last.end_time + duration
+
+    # USER METHODS TO HANDLE ENTRIES
+
+    def append(self, name: str, duration: timedelta, start: datetime = None) -> Entry:
+        start_time, end_time = self._start_end_time(duration, start)
+        result: Entry = Entry(name, start_time, end_time, duration)
+        self._entries.append(result)
+        return result
 
 
 if __name__ == '__main__':
-    start_time = datetime.now()
-    samples = 10
+    usr_synonyms = [
+        ('shower', 20),
+        ('snack', 10)
+    ]
 
-    first: Schedule = Schedule.from_names(
-        'Eating', 'Going to dinner', duration=timedelta(minutes=30))
+    schedule: Schedule = Schedule.from_entries(
+        'shower; 14:00', '30m', 'snack',
+        parser=StandardParser(synonyms=usr_synonyms, contingency=None)
+    )
 
-    #second: Schedule = Schedule.from_names(
-    #    'Breakfast', 'Movie', duration=timedelta(minutes=30))
-
-    print('\nDONE')
+    print(schedule)
